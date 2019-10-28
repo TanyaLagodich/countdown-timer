e5da0535d8c20932a53a20741c5c0042c41c62ac
Tanya <tatyana.lagodich@ovcenter.com>
2019-10-28 10:55:51 +0300

    new component InputBox

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
   <div id="app">
-    <div class="input-box">
-      <label>Название
-        <input type="text"
-              v-model="ev.name">
-      </label>
-      <label>Дата
-        <input type="date"
-              v-model="ev.date">
-      </label>
-      <label>Время
-        <input type="time"
-              v-model="ev.time">
-      </label>
-      <button @click="countLeftTime">Создать</button>
-    </div>
-
+    <input-box @countLeftTime="countLeftTime" />
     <h1>{{ leftTimes.days }} : {{ leftTimes.hours }} :
       {{ leftTimes.minutes }} : {{ leftTimes.seconds }}</h1>
 
   </div>
 </template>
 <script>
+import InputBox from './components/InputBox.vue';
+
 export default {
+  components: { InputBox },
   data() {
     return {
-      ev: {
-        name: '',
-        date: new Date(),
-        time: '00:00',
-      },
       leftTimes: {
         days: '',
         hours: '',
@@ -38,20 +21,10 @@ export default {
       },
     };
   },
-  created() {
-    this.ev.date = this.createEvDate();
-  },
   methods: {
-    createEvDate() {
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1;
-      const date = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
-      return `${year}-${month}-${date}`;
-    },
-    countLeftTime() {
+    countLeftTime(ev) {
       const today = new Date().getTime();
-      const eventDate = new Date(`${this.ev.date} ${this.ev.time}`).getTime();
+      const eventDate = new Date(`${ev.date} ${ev.time}`).getTime();
       const diff = eventDate - today;
       this.leftTimes.days = Math.floor(diff / (1000 * 60 * 60 * 24));
       this.leftTimes.hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
---
+++ src/components/InputBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="input-box">
+    <label>Название
+      <input type="text"
+             v-model="ev.name">
+    </label>
+    <label>Дата
+      <input type="date"
+             v-model="ev.date">
+    </label>
+    <label>Время
+      <input type="time"
+             v-model="ev.time">
+    </label>
+    <button @click="$emit('countLeftTime', ev)">Создать</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      ev: {
+        name: '',
+        date: new Date(),
+        time: '00:00',
+      },
+    };
+  },
+  created() {
+    this.ev.date = this.createEvDate();
+  },
+  methods: {
+    createEvDate() {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1;
+      const date = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+      return `${year}-${month}-${date}`;
+    },
+  },
+};
+</script>
