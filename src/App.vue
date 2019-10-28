30387ecc9fc9c6b6129f0ea2601f80774b671b20
Tanya <tatyana.lagodich@ovcenter.com>
2019-10-28 11:12:46 +0300

    counterLeft component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <input-box @countLeftTime="countLeftTime" />
-    <h1>{{ leftTimes.days }} : {{ leftTimes.hours }} :
-      {{ leftTimes.minutes }} : {{ leftTimes.seconds }}</h1>
-
+    <input-box v-if="!event"
+               @countLeftTime="countLeftTime" />
+    <count-left v-else
+                :leftTimes="leftTimes" />
   </div>
 </template>
 <script>
 import InputBox from './components/InputBox.vue';
+import CountLeft from './components/CountLeft.vue';
 
 export default {
-  components: { InputBox },
+  components: { InputBox, CountLeft },
   data() {
     return {
+      event: false,
       leftTimes: {
         days: '',
         hours: '',
@@ -23,6 +25,7 @@ export default {
   },
   methods: {
     countLeftTime(ev) {
+      this.event = true;
       const today = new Date().getTime();
       const eventDate = new Date(`${ev.date} ${ev.time}`).getTime();
       const diff = eventDate - today;
@@ -31,8 +34,7 @@ export default {
       this.leftTimes.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       this.leftTimes.seconds = Math.floor((diff % (1000 * 60)) / 1000);
       let timerId = setInterval(() => {
-        console.log(diff < 0, timerId);
-        timerId = setInterval(this.countLeftTime, 1000);
+        timerId = setInterval(() => this.countLeftTime(ev), 1000);
         if (diff === 0 || diff < 0) clearInterval(timerId);
       }, 0);
     },
---
+++ src/components/CountLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ leftTimes.days }} : {{ leftTimes.hours }} :
+        {{ leftTimes.minutes }} : {{ leftTimes.seconds }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    leftTimes: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
